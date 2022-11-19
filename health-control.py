f73fdfd2d9c6a4a0a8cf777605e0ce2a4b3763a7
Eduardo Lima <eduardolima100@gmail.com>
2022-11-19 13:24:25 -0300

    update health_control.py

--- health-control.py
+++ health-control.py
@@ -14,6 +14,9 @@ class Proficional:
                 'telefone': self.telefone,
                 'cidade': self.cidade}
 
+    def register():
+        pass
+
     def __str__(self):
         return '{} {}'.format(self.especialidade, self.nome)
 
@@ -45,6 +48,9 @@ class Medicacao:
                 'duracao': self.duracao,
                 'condicao': self.condicao}
 
+    def register():
+        pass
+
     def __str__(self):
         qnt = '\t{}'.format(self.qnt) if self.qnt != None else ''
         un = self.un if self.un != None else ''
@@ -67,6 +73,9 @@ class Receita:
         return {'proficional': self.proficional.as_dict(),
                 'data': self.data,
                 'medicacao': medicacao}
+
+    def register():
+        pass
 
     def __str__(self):
         s = '{}\n{}\n\n'.format(self.data, self.proficional)
@@ -98,6 +107,9 @@ class Sintoma:
                 'un': self.un,
                 'qlt': self.qlt}
     
+    def register():
+        pass
+
     def __str__(self):
         return self.nome
 
@@ -109,6 +121,9 @@ class Doenca:
     def as_dict(self):
         return {'nome': self.nome,
                 'data': self.data}
+
+    def register():
+        pass
 
     def __str__(self):
         return self.nome
@@ -123,6 +138,9 @@ class Exame:
         return {'nome': self.nome,
                 'medico': self.medico,
                 'data': self.data}
+
+    def register():
+        pass
 
     def __str__(self):
         return self.nome
@@ -147,7 +165,10 @@ class Consulta:
                 'data': self.data,
                 'receita': self.receita.as_dict(),
                 'diagnostico': self.diagnostico.as_dict(),
-                'exame': self.exame.as_dict()}
+                'exame': self.exame.as_dict() if self.exame != None else None}
+
+    def register():
+        pass
 
     def __str__(self):
         return '{} | {} | {}'.format(self.data, self.proficional, self.local)
@@ -168,10 +189,10 @@ class Paciente:
         if altura != None:  self.add_altura(altura)
     
     def add_peso(self, peso: float, data:date = date.today()):
-        self.peso[len(self.peso)] = {'data:': data, 'peso': peso}
+        self.peso[len(self.peso)] = {'data:': data, 'valor': peso}
     
     def add_altura(self, altura, data = date.today()):
-        self.altura[len(self.altura)] = {'data:': data, 'peso': altura}
+        self.altura[len(self.altura)] = {'data:': data, 'valor': altura}
     
     def add_proficional(self, proficional: Proficional):
         self.proficionais[len(self.proficionais)] = proficional
@@ -197,6 +218,9 @@ class Paciente:
                 'altura': self.altura,
                 'consultas': consultas}
 
+    def register():
+        pass
+
     def __str__(self):
         return self.nome
 
@@ -210,7 +234,7 @@ def use_case():
     data = date(2022, 10, 24)
 
     sintomas = {}
-    sintomas[0] = Sintoma('febre', data, 38.3)
+    sintomas[0] = Sintoma('febre', data, 38.3, un='ºC')
     sintomas[1] = Sintoma('dor de cabeça', data)
 
     medico = Proficional('Eduarda Dalcin Marques', 'Médica', 'Santa Maria')
@@ -236,6 +260,34 @@ def use_case():
         print(paciente.consultas[k].receita)
         print('Diagnostico:', paciente.consultas[k].diagnostico)
 
+def json_model():
+    paciente = Paciente('Eduardo Machado de Lima', date(1997, 8, 13), 70.9, 1.73)
+
+    data = date(2022, 10, 24)
+
+    sintomas = {}
+    sintomas[0] = Sintoma('febre', data, 38.3, un='ºC')
+    sintomas[1] = Sintoma('dor de cabeça', data)
+
+    medico = Proficional('Eduarda Dalcin Marques', 'Médica', 'Santa Maria')
+    medicamentos = {}
+    medicamentos[0] = Medicacao('Amoxicilina 500mg + Clavulanato 125mg',30, 'comp', 8, 10)
+    medicamentos[1] = Medicacao('Lisador', 1, 'caixa', 6, condicao='dor ou febre')
+    medicamentos[2] = Medicacao('Predisim 20mg', 1, 'caixa', 12, 5)
+    medicamentos[3] = Medicacao('Rinosoro Jet', 1, 'frasco', 8, condicao='sintomas')
+    medicamentos[4] = Medicacao('Avamys', 1, 'frasco', 24, 30, condicao='noite')
+    
+    receita = Receita(medico, data, medicamentos)
+
+    diagnostico = Doenca('Sinusite', data)
+    
+    consulta = Consulta(medico, 'Hospital da Brigada', sintomas, data, receita, diagnostico)
+
+    paciente.add_consulta(consulta)
+
+    print('Paciente\n', paciente.as_dict())
+
+
 def main():
     try:
         js = open('js.json', 'r')
@@ -245,10 +297,32 @@ def main():
     print('Controle de Saúde:')
     print('Opções: 1- Acessar; 2- Cadastrar')
     inp = input('> ')
-    if inp == 1:
-        pass
-    elif inp == 2:
-        pass
-
+    if inp == '1':
+        print('Acessar:\n1- Proficional\n2- Medicação\n3- Receita\n4- Sintoma\n5- Doença\n6- Exame\n7- Consulta\n8- Paciente')
+        inp = input('> ')
+    elif inp == '2':
+        print('Cadastrar:\n1- Proficional\n2- Medicação\n3- Receita\n4- Sintoma\n5- Doença\n6- Exame\n7- Consulta\n8- Paciente')
+        inp = input('> ')
+        if inp == '1':
+            Proficional.register()
+        elif inp == '2':
+            Medicacao.register()
+        elif inp == '3':
+            Receita.register()
+        elif inp == '4':
+            Sintoma.register()
+        elif inp == '5':
+            Doenca.register()
+        elif inp == '6':
+            Exame.register()
+        elif inp == '7':
+            Consulta.register()
+        elif inp == '8':
+            Paciente.register()
 #main()
-use_case()
+json_model()
+#use_case()
+
+# Falta:
+# Fazer fuções de registro
+# Abrir e salvar json
